4bc7787c329b9fed5a980020d4a8761f97697f18
michkrzeminski <michkrzeminski@gmail.com>
2019-05-04 18:52:33 +0200

    clearing old application

--- src/events/services/data.ts
+++
@@ -1,9 +0,0 @@
-import {BaseData} from '../../utils/services/data';
-
-export interface ScheduleEvent extends BaseData {
-
-    StartTime: string;
-    EndTime: string;
-    Title: string;
-
-}
--- src/utils/services/data.ts
+++
@@ -1,5 +0,0 @@
-export interface BaseData {
-
-    ID?: number;
-
-}
